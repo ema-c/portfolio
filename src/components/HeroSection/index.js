9cf9ef7adecd7689a70af6c56d55d35de6393d66
ema-c <emacojan@gmail.com>
2022-09-27 18:00:45 +0100

    3

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from "./HeroElements";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElement";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Emanuela Ciobanu</HeroH1>
+        <HeroP>Web developer developing </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseHover={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    id: "about",
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: "lorem ipsum",
+    headline: "lorem ipsum",
+    description: "lorem ipsum",
+    buttonLabel: "Glorem ipsum",
+    imgStart: false,
+    img: require("../../images/svg-1.png"),
+    alt: "lorem ipsum",
+    dark: true,
+    primary: true,
+    darkText: false
+  };
+  
+  export const homeObjTwo = {
+    id: "discover",
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "lorem ipsum",
+    headline: "lorem ipsum",
+    description: "lorem ipsum",
+    buttonLabel: "lorem ipsum",
+    imgStart: true,
+    img: require("../../images/svg-2.png"),
+    alt: "lorem ipsum",
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+  
+  export const homeObjThree = {
+    id: "services",
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: "lorem ipsim",
+    headline: "lorem ipsim",
+    description: "lorem ipsim",
+    buttonLabel: "lorem ipsim",
+    imgStart: false,
+    img: require("../../images/svg-3.png"),
+    alt: "lorem ipsim",
+    dark: true,
+    primary: true,
+    darkText: false
+  };
+  
